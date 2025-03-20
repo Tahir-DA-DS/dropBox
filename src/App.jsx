cfcf3ae2ce2c41380c41e1572ac6aa7b6d90615f
Tahir-DA-Ds <tahir.adeleye@gmail.com>
2025-03-20 15:00:59 +0100

    storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Amplify} from "aws-amplify";
+// import { Amplify} from "aws-amplify";
+// import { Storage } from "@aws-amplify/storage";
+// import awsExports from "./aws-exports";
+import { Amplify } from "aws-amplify";
 import { Storage } from "@aws-amplify/storage";
 import awsExports from "./aws-exports";
 import { withAuthenticator } from "@aws-amplify/ui-react";
